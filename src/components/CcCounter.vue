515c4db7ac635fb7e540fd720a58a23aafdca02c
Christophe <christophevelou@gmail.com>
2021-04-08 20:07:42 +0200

    first commit - Compteur sans vueX

---
+++ src/components/CcCounter.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2>Un Simple compteur sans vueX</h2>
+    <h2>{{ currentValue }}</h2>
+    <div>
+      <button @click="increment">+</button>
+      <button @click="decrement">-</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentValue: 0
+    };
+  },
+  methods: {
+    increment() {
+      this.currentValue = this.currentValue + 1;
+    },
+    decrement() {
+      this.currentValue = this.currentValue - 1;
+    }
+  }
+};
+</script>
+
+<style scoped></style>
